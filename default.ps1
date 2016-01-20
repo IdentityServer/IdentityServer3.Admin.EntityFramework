f47f1e8c3ce0226bd74a65eb163408cf1c7a4aec
Bert Hoorne <hoorneb@gmail.com>
2016-01-20 21:13:42 +0100

    Support for ps build, added nuspec & nuget  dependecy fix

---
+++ default.ps1
@@ -0,0 +1,80 @@
+properties {
+	$base_directory = Resolve-Path . 
+	$src_directory = "$base_directory\source"
+	$output_directory = "$base_directory\build"
+	$dist_directory = "$base_directory\distribution"
+	$sln_file = "$src_directory\IdentityServer3.Admin.EntityFramework.sln"
+	$target_config = "Release"
+	$framework_version = "v4.5"
+	$xunit_path = "$src_directory\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+	$ilmerge_path = "$src_directory\packages\ilmerge.2.14.1208\tools\ILMerge.exe"
+	$nuget_path = "$src_directory\.nuget\nuget.exe"
+	
+	$buildNumber = 0;
+	$version = "1.0.0.0"
+	$preRelease = $null
+}
+
+task default -depends Clean, CreateNuGetPackage
+task appVeyor -depends Clean, CreateNuGetPackage
+
+task Clean {
+	rmdir $output_directory -ea SilentlyContinue -recurse
+	rmdir $dist_directory -ea SilentlyContinue -recurse
+	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
+}
+
+task Compile -depends UpdateVersion {
+		exec { msbuild /nologo /verbosity:q $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5 }
+
+	if ($LastExitCode -ne 0) {
+        exit $LastExitCode
+    }
+}
+
+task UpdateVersion {
+	$vSplit = $version.Split('.')
+	if($vSplit.Length -ne 4)
+	{
+		throw "Version number is invalid. Must be in the form of 0.0.0.0"
+	}
+	$major = $vSplit[0]
+	$minor = $vSplit[1]
+	$patch = $vSplit[2]
+	$assemblyFileVersion =  "$major.$minor.$patch.$buildNumber"
+	$assemblyVersion = "$major.$minor.0.0"
+	$versionAssemblyInfoFile = "$src_directory/VersionAssemblyInfo.cs"
+	"using System.Reflection;" > $versionAssemblyInfoFile
+	"" >> $versionAssemblyInfoFile
+	"[assembly: AssemblyVersion(""$assemblyVersion"")]" >> $versionAssemblyInfoFile
+	"[assembly: AssemblyFileVersion(""$assemblyFileVersion"")]" >> $versionAssemblyInfoFile
+}
+
+task CreateNuGetPackage -depends Compile {
+	$vSplit = $version.Split('.')
+	if($vSplit.Length -ne 4)
+	{
+		throw "Version number is invalid. Must be in the form of 0.0.0.0"
+	}
+	$major = $vSplit[0]
+	$minor = $vSplit[1]
+	$patch = $vSplit[2]
+	$packageVersion =  "$major.$minor.$patch"
+	if($preRelease){
+		$packageVersion = "$packageVersion-$preRelease" 
+	}
+
+	if ($buildNumber -ne 0){
+		$packageVersion = $packageVersion + "-build-" + $buildNumber.ToString().PadLeft(5,'0')
+	}
+	
+  md $dist_directory
+  md $dist_directory\lib
+  md $dist_directory\lib\net45
+  
+  copy-item $src_directory\IdentityServer3.Admin.EntityFramework\IdentityServer3.Admin.EntityFramework.nuspec $dist_directory
+  copy-item $output_directory\IdentityServer3.Admin.EntityFramework.dll $dist_directory\lib\net45
+  copy-item $output_directory\IdentityServer3.Admin.EntityFramework.pdb $dist_directory\lib\net45
+
+	exec { . $nuget_path pack $dist_directory\IdentityServer3.Admin.EntityFramework.nuspec -BasePath $dist_directory -o $dist_directory -version $packageVersion }
+}
